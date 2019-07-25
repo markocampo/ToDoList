c5e60748241b8b4b8d231368dd5188ccb267690f
markocampo <46905054+markocampo@users.noreply.github.com>
2019-07-25 17:34:11 -0600

    Add files via upload

---
+++ assets/js/todos.js
@@ -0,0 +1,28 @@
+//Check off speicifc todos by clicking
+$('ul').on('click', 'li', function(){
+	$(this).toggleClass('completed');
+});
+
+//click on x to delete to do
+$('ul').on('click', 'span', function(e){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	e.stopPropagation();
+});
+
+//add keypress function to input
+$('input[type="text"]').keypress(function(e){
+	if(e.which === 13){
+		//grab new todo text from input
+		var todoText = $(this).val();
+		//clear input
+		$(this).val('');
+		//create a new li and add to ul
+		$('ul').append('<li><span><i class="fa fa-trash"></i></span> ' + todoText + '</li>')
+	}
+});
+
+$('.fa-plus').click(function(){
+	$('input[type="text"]').fadeToggle()
+});
